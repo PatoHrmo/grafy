db1373d994ff1409dbf0437376b5c2eda6f472b2
Kristian Chudjak <Chudjakk@gmail.com>
2016-11-28 23:24:58 +0100

    pridal som pikolína a gui

---
+++ src/gui/Hrana.java
@@ -0,0 +1,37 @@
+import edu.umd.cs.piccolo.PNode;
+import edu.umd.cs.piccolo.nodes.PPath;
+
+import java.awt.*;
+import java.awt.geom.Point2D;
+
+/**
+ * Created by Chudjak Kristián on 20.10.2016.
+ */
+public class Hrana extends PPath{
+    private Vrchol vrchol01;
+    private Vrchol vrchol02;
+
+    public Hrana(Vrchol v1, Vrchol v2){
+        this.vrchol01 = v1;
+        this.vrchol02 = v2;
+
+        this.setStroke(new BasicStroke(5));
+
+    }
+
+    public void paint(){
+
+        Point2D start = vrchol01.getFullBoundsReference().getCenter2D();
+        Point2D end = vrchol02.getFullBoundsReference().getCenter2D();
+
+        this.reset();
+        this.moveTo((float)start.getX(), (float)start.getY());
+        this.lineTo((float)end.getX(), (float)end.getY());
+
+    }
+
+    public boolean obsahujeVrchol(Vrchol v){
+        return vrchol01 == v || vrchol02 == v;
+    }
+
+}
